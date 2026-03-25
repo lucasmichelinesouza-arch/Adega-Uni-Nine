fcd8753d56cbfe5d189b637959fd4ded06845b30
cadicco <cadicco@gmail.com>
2026-03-25 01:15:24 +0000

    fiz inclusao dos elementos em pastas apenas para melhor organizaçao, espero nao ter atrapalhado

--- php/form.php
+++ php/form.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title> <link rel="stylesheet" href="form.css">
+    <title>Document</title> <link rel="stylesheet" href="css/form.css">
 </head>
 <body>
-    <img src="img_adega.jpeg">
+    <img src="imagens/img_adega.jpeg">
     <h1>Adega UniNine</h1>
-    <form method="post" action="recebe.php">
+    <form method="post" action="php/recebe.php">
     Login  <input type="text" id="login" name="login"><br><br>
     Senha <input type="password" id="senha" name="senha"><br><br>
     <div class="entrar"><input type="submit" value="Entrar"></div>
