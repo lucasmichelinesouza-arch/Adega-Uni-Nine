38325dc34dbf0101f27d42f7fb35259990503087
Gustavo_Nunes <gustavo.r.sousa@icloud.com>
2026-04-07 21:27:28 -0300

    Update form.php

--- php/form.php
+++ php/form.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title> <link rel="stylesheet" href="css/form.css">
+    <title>Document</title> <link rel="stylesheet" href=".css">
 </head>
 <body>
-    <img src="imagens/img_adega.jpeg">
+    <img src="imagens/img_adega (1).jpeg">
     <h1>Adega UniNine</h1>
-    <form method="post" action="php/recebe.php">
+    <form method="post" action="recebe.php">
     Login  <input type="text" id="login" name="login"><br><br>
     Senha <input type="password" id="senha" name="senha"><br><br>
     <div class="entrar"><input type="submit" value="Entrar"></div>
