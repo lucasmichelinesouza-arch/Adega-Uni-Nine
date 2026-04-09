b7134909421c250753348fb6bfd88072d916c545
CADICCO <cadicco@gmail.com>
2026-04-09 00:04:49 -0300

    precisei alterar caminho para imagem e css, nao estava abrindo. tbm a pasta no htdocs de ter o nome Adega-Uni-Nine para não dar erro geral

--- php/form.php
+++ php/form.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title> <link rel="stylesheet" href=".css">
+    <title>Document</title>
+    <link rel="stylesheet" href="/Adega-Uni-Nine/php/form.css">
 </head>
 <body>
-    <img src="imagens/img_adega (1).jpeg">
+    <img src="/Adega-Uni-Nine/imagens/img_adega.jpeg">
     <h1>Adega UniNine</h1>
     <form method="post" action="recebe.php">
     Login  <input type="text" id="login" name="login"><br><br>
     Senha <input type="password" id="senha" name="senha"><br><br>
     <div class="entrar"><input type="submit" value="Entrar"></div>
-
- </form>
+    </form>
 </body>
 </html>
